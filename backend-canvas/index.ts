e4a20fa7dce804de7029fbb401e35ca32b1605dc
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-03-12 16:53:47 +0600

    add Type to DRAW

--- backend-canvas/index.ts
+++ backend-canvas/index.ts
@@ -1,7 +1,7 @@
 import expressWs from "express-ws";
 import express from 'express';
 import cors from 'cors';
-import {ActiveConnections} from "./types";
+import {ActiveConnections, CanvasDraw, IncomingMessage} from "./types";
 
 const app = express()
 const port = 8000;
@@ -12,12 +12,31 @@ app.use(cors());
 const canvasRouter = express.Router();
 
 const activeConnection: ActiveConnections = {};
+let canvasDrawData: CanvasDraw[] = [];
 
-canvasRouter.ws('/', (ws, req) => {
+canvasRouter.ws('/', (ws, _req) => {
     const id = crypto.randomUUID();
     console.log(`Client connected id=${id}`);
 
     activeConnection[id] = ws;
+    ws.send(JSON.stringify({type: 'WELCOME', payload: canvasDrawData}));
+
+    ws.on('message', (message) => {
+        const parsedMessage = JSON.parse(message.toString()) as IncomingMessage;
+        canvasDrawData.push(parsedMessage.payload);
+
+        if (parsedMessage.type === 'DRAW') {
+            Object.values(activeConnection).forEach(connection => {
+                const outgoingCanvasDraw = {type: 'DRAW', payload: canvasDrawData};
+                connection.send(JSON.stringify(outgoingCanvasDraw));
+            })
+        }
+    });
+
+    ws.on('close', () => {
+        console.log(`User disconnected id=${id}`);
+        delete activeConnection[id];
+    });
 });
 
 app.use('/canvas', canvasRouter);
--- backend-canvas/types.d.ts
+++ backend-canvas/types.d.ts
@@ -8,3 +8,8 @@ export interface CanvasDraw {
     x: string;
     y: string;
 }
+
+export interface IncomingMessage {
+    type: string;
+    payload: CanvasDraw;
+}
